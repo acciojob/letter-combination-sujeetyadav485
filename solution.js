6a1e0b2af22c963f0d247a6690da6906f1104091
Sujeet Yadav <99132296+sujeetyadav485@users.noreply.github.com>
2022-12-16 01:12:24 +0530

    Sujeet Submission

--- solution.js
+++ solution.js
@@ -1,5 +1,32 @@
-function letterCombinations(input_digit) {
+function letterCombinations(digits) {
   //Complete the function
+  if (digits == null || digits.length === 0) return [];
+
+  const map = {
+    2: 'abc',
+    3: 'def',
+    4: 'ghi',
+    5: 'jkl',
+    6: 'mno',
+    7: 'pqrs',
+    8: 'tuv',
+    9: 'wxyz',
+  };
+
+  const res = [];
+  const go = (i, s) => {
+    if (i === digits.length) {
+      res.push(s);
+      return;
+    }
+
+    for (const c of map[digits[i]]) {
+      go(i + 1, s + c);
+    }
+  };
+
+  go(0, '');
+  return res;
 }
 
 module.exports = letterCombinations;
